357686f26d7b608e16d9f164825debb87a3eeb91
CaptainJRoy <jrsmiguel@outlook.pt>
2023-12-13 13:05:44 +0000

    master - Add day12 part1

---
+++ day12/part2.js
@@ -0,0 +1,61 @@
+import { readFileSync } from 'fs';
+
+
+function run() {
+    const lines = readFileSync('./day12/input/input2.txt', 'utf-8').split('\n');
+
+    let sum = 0;
+    lines.forEach(line => {
+        let [conditions, check] = line.split(' ');
+        conditions = conditions + conditions + conditions + conditions + conditions;
+        check = check + ',' + check + ',' + check + ',' + check + ',' + check;
+        // console.log(conditions, check)
+
+        let singleConditionExpansion = [];
+        conditions.split('').forEach(cond => {
+            if (cond === '?') {
+                singleConditionExpansion = singleConditionExpansion.concat(singleConditionExpansion);
+                if (singleConditionExpansion.length === 0) {
+                    singleConditionExpansion.push('.');
+                    singleConditionExpansion.push('#');
+                }
+                else {
+                    let temp = singleConditionExpansion.slice();
+                    for (let i = 0; i < singleConditionExpansion.length; i++) {
+                        singleConditionExpansion[i] = singleConditionExpansion[i] + '.';
+                    }
+                    for (let i = 0; i < temp.length; i++) {
+                        temp[i] = temp[i] + '#';
+                    }
+                    singleConditionExpansion = singleConditionExpansion.concat(temp);
+                }
+            }
+            else {
+                if (singleConditionExpansion.length === 0) {
+                    singleConditionExpansion.push(cond);
+                }
+                else {
+                    for (let i = 0; i < singleConditionExpansion.length; i++) {
+                        singleConditionExpansion[i] = singleConditionExpansion[i] + cond;
+                    }
+                }
+            }
+
+            singleConditionExpansion = [...new Set(singleConditionExpansion)];
+        });
+
+        let temp = singleConditionExpansion.filter(cond => {
+            let fields = check.split(',').map(field => parseInt(field));
+            let match = cond.split('.').filter(field => field.length !== 0).map(field => field.length);
+
+            return fields.length === match.length && fields.every((val, index) => val === match[index]);
+        });
+        
+        sum += temp.length;
+    });
+
+    return sum;
+}
+
+
+export default run;
